a771baa36d606de01cd9683c060c448a2a4ee18c
NickyBBCN <nicky025@gmail.com>
2022-02-06 21:33:10 -0500

    agregamos el proyecto

---
+++ Clientes/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Clientes
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-2EJEG1D ;Initial Catalog = CLIENTES-B;Trusted_Connection=true;");
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Nombre = textBox1.Text;
+            string tipo =  comboBox1.Text;
+            string cedula = textBox3.Text;
+            string direccion = textBox4.Text;
+            string telefono = textBox5.Text;
+            string correo = textBox5.Text;
+            string fecha_creacion = dateTimePicker1.Text;
+            //string address = textBox4.Text;
+        }
+    }
+}
